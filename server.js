1746746d6128790098ec64c17a0b7a684c9c22c7
savithabj <savithabj@gmail.com>
2014-12-22 01:09:52 +0000

    inspirational server

---
+++ server.js
@@ -0,0 +1,30 @@
+var express = require("express"),
+  app = express();
+  
+  app.get("/", function (req, res) { 
+      res.send("Hello Universe!!");
+  });
+  
+  var inspireArr = ["The answer to life the universe and everthing is 42", 
+     "Quick brown fox jumped over the lazy dog"];
+  
+  app.get("/inspiration", function (req, res) { 
+      var randReturn = Math.floor(Math.random()*inspireArr.length);
+      res.send(inspireArr[randReturn]);
+  });
+  
+  function JokeObj (s1, p1) {
+      this.setup = s1;
+      this.punchline = p1;
+  }
+  var jokeArr = [new JokeObj("abc ?", "xyz"), 
+    new JokeObj("mno ?", "def"), 
+    new JokeObj("pqr ?", "uvw") ];
+  
+  app.get("/joke", function (req, res) { 
+      var randReturn = Math.floor(Math.random()*jokeArr.length);
+      res.json(jokeArr[randReturn]);
+  });
+  
+  console.log("server starting. available at http://localhost:3000");
+  app.listen(3000);
